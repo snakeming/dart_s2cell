efcc53b382c988d35ce671ef0150364f9db5fd49
Coder <copilotcoder@gmail.com>
2024-03-02 10:35:25 +0800

    initial commit
    minor fix based on https://github.com/nbspou/dart-s2geometry

---
+++ lib/dart_s2cell.dart
@@ -0,0 +1,10 @@
+library dart_s2cell;
+
+export 's2cell_id.dart';
+export 's2latlng.dart';
+
+/// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
