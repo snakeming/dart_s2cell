1bb22c4cb27d90edc1bb0c6825ce650c806ee6f3
Coder <copilotcoder@gmail.com>
2025-02-23 13:01:01 +0800

    upgrade to flutter SDK 3.5

---
+++ lib/package_config.dart
@@ -0,0 +1,9 @@
+
+class PackageConfig{
+  // domain code: 221
+  static const domainPrefix='DSC'; 
+
+  // ddn't assume this package depends on logger
+  // static final SmLoggerInterface logger =  SmLog.getLogger(shortName);
+  
+}
---
+++ lib/package_error_code.dart
@@ -0,0 +1,10 @@
+
+// error code standard format is 6 digits, 3 digits prefix for error domain
+// and 3 digits for error code
+// dart_s2cell use domain code 221
+class PackageErrorCode{
+
+  // 
+  // static const dummyError = 221000;
+  
+}
